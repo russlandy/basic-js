12d689c3e8a5d52593d8889b6ba470fe5b0eca75
russlandy <ruslandabysov@gmail.com>
2023-04-16 13:15:38 +0300

    feat: solve repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  // let arr = new Map (Object.entries(options));
+  // let strStart = String(str);
+  // let additionSeparator = String(arr.get('additionSeparator'));
+  // let additionRepeatTimes = Number(arr.get('additionRepeatTimes'));
+  // let addition = String(arr.get('addition'));
+  // let separator = String(arr.get('separator'));
+  // let repeatNum = Number(arr.get('repeatTimes'));
+  // let addString = '';
+  // addString = additionSeparator + addition;
+  // let addStrRepeat = addString.repeat(additionRepeatTimes).slice(additionSeparator.length);
+  // let totalStr = separator + strStart + addStrRepeat;
+  // if (Number.isInteger(repeatNum)) {totalStr = totalStr.repeat(repeatNum).slice(separator.length)};
+  // return totalStr
+  const repeatArr = []
+  
+  let additionSeparator = options.additionSeparator !== undefined ? options.additionSeparator: '|';
+  let additionRepeatTimes = options.additionRepeatTimes !== undefined ? options.additionRepeatTimes: 1;
+  let addition = options.addition !== undefined ? String(options.addition): '';
+  let separator = options.separator !== undefined ? options.separator: '+';
+  let repeatNum = options.repeatTimes !== undefined ? options.repeatTimes: 1;
+  let newstr = String(str);
+  
+  
+  for (let i = 0; i < repeatNum; i++) {
+      if (newstr !== undefined && newstr !== null) {
+        repeatArr.push(String(newstr))
+      }
+      for (let k = 0; k < additionRepeatTimes; k++) {
+        if (addition !== undefined && addition !== null) {
+          repeatArr.push(addition);
+        }
+        if (k < additionRepeatTimes - 1 && addition !== undefined && addition !== null) {
+          repeatArr.push(additionSeparator);
+        }
+      }
+      if (i < repeatNum - 1 && newstr !== null && newstr !== undefined) {
+        repeatArr.push(separator);
+      } 
+    }
+    return repeatArr.join('');
+
+    
 }
+
+
 
 module.exports = {
   repeater
