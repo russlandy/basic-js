e50cc86435ed1f91dd5c605f4fe6611c3db9b490
russlandy <ruslandabysov@gmail.com>
2023-04-17 11:49:39 +0300

    feat: right solve of common character

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -26,7 +26,9 @@ function getCommonCharacterCount(s1, s2) {
   let counter = 0;
   for (let i=0; i < s1.length; i++) {
     if(s2.indexOf(s1[i]) !== -1 ) {
-      counter++
+      counter++;
+      s2 = s2.replace(s1[i], '');
+
     }
   }
   return counter
